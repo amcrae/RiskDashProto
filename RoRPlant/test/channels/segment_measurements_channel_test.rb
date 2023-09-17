313f6f68e9a101148fbb10fc0086f43ce376657c
Andrew McRae <ajmcrae@gmail.com>
2023-09-18 01:00:24 +1000

    Generated channel files

---
+++ RoRPlant/test/channels/segment_measurements_channel_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class SegmentMeasurementsChannelTest < ActionCable::Channel::TestCase
+  # test "subscribes" do
+  #   subscribe
+  #   assert subscription.confirmed?
+  # end
+end
