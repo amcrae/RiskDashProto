de06aa4ad63bd6d9e386f51fd2d21e210dc69ee4
Andrew McRae <ajmcrae@gmail.com>
2023-09-18 00:03:50 +1000

    Hierarchical query ORM and Tree-like presentation.
    * alternative method of mapping rows to objects allows caclulated
    (non-table) fields to be set.
    * tree-like presentation using variable left margin.

--- RoRPlant/app/controllers/plant_controller.rb
+++ RoRPlant/app/controllers/plant_controller.rb
@@ -8,7 +8,7 @@ class PlantController < ApplicationController
 	     SELECT level + 1 AS level, (connect_by_path || '/' || s.shortname) AS connect_by_path, s.* 
 	       FROM cte_connect_by r INNER JOIN segments s ON  r.id = s.parent_id
 	  )
-	  SELECT level, id, segtype, shortname, parent_id
+	  SELECT level, id, segtype, shortname, parent_id, connect_by_path
 	  FROM cte_connect_by
 	  ORDER BY connect_by_path
     HEREDOC
@@ -16,8 +16,20 @@ class PlantController < ApplicationController
     # level, connect_by_path path
     plain_sql = " SELECT id, segtype, shortname, parent_id from segments ORDER BY id"
     
-    @seg_tree = Segment.find_by_sql(hierarchy_sql)
-    Rails.logger.info("**************** got data")
+    # @seg_tree = Segment.find_by_sql(hierarchy_sql)
+    res = Segment.connection.select_all(hierarchy_sql).to_a();
+    Rails.logger.info("************* got data")
+    Rails.logger.info(res)
+    Rails.logger.info("***")
+    tree_order = []
+    for r in res do
+      puts r;
+      seg = Segment.new(id:r["id"], shortname:r["shortname"], segtype:r["segtype"], parent_id:r["parent_id"]);
+      seg.level = r["level"]
+      seg.path = r["connect_by_path"]
+      tree_order.push(seg)
+    end
+    @seg_tree = tree_order
 
   end
 
