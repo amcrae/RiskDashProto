d06c84e2a401d8dc72ad42822c3cea5e05eedbad
Andrew McRae <ajmcrae@gmail.com>
2023-09-20 01:46:49 +1000

    Write synthetic measurements to database.
    + Separate synthesis and broadcasting.
    + Synthesiser writes data to database as this is the easiest transfer
    method available.
    + choice of running synthesis in external delayed_job process or as a
    forked web server.

--- RoRPlant/app/controllers/synth_controller.rb
+++ RoRPlant/app/controllers/synth_controller.rb
@@ -1,7 +1,21 @@
 class SynthController < ActionController::API
+
+  SYNTH_THREAD_STYLES = ['EASE_OF_USE', 'EXTERNAL_PROCESS'];
+
+  USE_SYNTH_THREAD_STYLE = 'EASE_OF_USE';
+
   def restart_synth
-    SynthesiseDataJob.new().run_in_thread()
-    redirect_to root_path, status: :see_other  
+    # SynthesiseDataJob.new().run_in_thread()
+    if (USE_SYNTH_THREAD_STYLE == 'EASE_OF_USE')
+    	Rails.logger.info("Starting job workers as child process in this web server.");
+        SynthesiseDataJob.new().run_as_new_process();
+    elsif (USE_SYNTH_THREAD_STYLE == 'EXTERNAL_PROCESS')
+    	Rails.logger.info("Use of external job worker process is assumed.");
+    	SynthesiseDataJob.perform_later();
+    else 
+    	Rails.logger.error("Unknown job thread style!");    
+    end
+    # redirect_to root_path, status: :see_other
   end
 
   def stop_synth
--- RoRPlant/app/jobs/synthesise_data_job.rb
+++ RoRPlant/app/jobs/synthesise_data_job.rb
@@ -2,23 +2,49 @@ require "action_cable"
 
 class SynthesiseDataJob < ApplicationJob
   queue_as :default
+
+  # try to ensure only one worker pool runs all synth jobs
+  @@singleton_process = nil
   
   attr_accessor :keep_running, :last_value
 
   # TODO: should be configurable by caller of perform. hardcoded for demo expediency.
   MLOC_DATA_DISTRIBUTIONS = [
     {mloc_uuid:"1-2dfe-2-1", avg_val:15.0, phase: 0.0, template:{qtype:"Current", uom:"A"} },
-    # {mloc_uuid:"1-2f33-2-1", avg_val:14.9, phase: 7.3, template:{qtype:"Current", uom:"A"} }
+    # {mloc_uuid:"1-2f33-2-1", avg_val:14.9, phase: 500.0, template:{qtype:"Current", uom:"A"} }
   ]
+  
+  def SynthesiseDataJob.singleton_process()
+  	return @@singleton_process
+  end
+
+  def SynthesiseDataJob.singleton_process=(new_proc)
+  	@@singleton_process = new_proc
+  end
+  
 
   def initialize(*args)
     super(*args)
     @mlocs = {}
     MLOC_DATA_DISTRIBUTIONS.each do |mdist|
-      mloc = MLocation.find_by(uuid:mdist[:mloc_uuid]);
-      @mlocs[mloc.uuid] = mloc
+      # Rails.logger.debug("Find #{mdist}");
+      muuid = mdist[:mloc_uuid];
+      mloc = MLocation.find_by(uuid:muuid);
+      if mloc == nil then
+      	Rails.logger.error("no mloc for #{mdist}");
+      end
+      # Rails.logger.debug("Found #{mloc}");
+      @mlocs[mloc.uuid] = mloc;
     end
+
   end
+
+
+  def run_as_queued_job()
+    j = SynthesiseDataJob.perform_later();
+    puts "perform_later => ", j
+  end
+
 
   def run_in_thread()
     # Thread.new(4, 15.0) { |segid, loc_name, avg_val|
@@ -29,15 +55,44 @@ class SynthesiseDataJob < ApplicationJob
     }
   end
 
+  
+  def run_as_new_process()
+    SynthesiseDataJob.perform_later();
+    puts "singleton_process == #{SynthesiseDataJob.singleton_process()}";
+    # TODO: figure out why @@singleton_process is nil after the process was started.
+    if SynthesiseDataJob.singleton_process() != nil then
+    	puts "SynthesiseDataJob singleton apparently already started.";
+    	Rails.logger.warn("SynthesiseDataJob singleton apparently already started.");
+    	return
+    end
+    job = self
+    fork do
+	# require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
+	require 'delayed/command'
+	SynthesiseDataJob.singleton_process = Delayed::Command.new(["run","--exit-on-complete"]);
+    	puts "singleton_process == #{SynthesiseDataJob.singleton_process()}";
 
-  def gen_measurement_for(mdist, simtime)
-    phasedt = simtime + mdist[:phase];
-    last_value = mdist[:avg_val] + (0.66*@n1d[phasedt] + 0.34*@n1d[phasedt*3.2] - 0.5);
-    logger.debug(@last_value);
+	# @@singleton_process.daemonize()  # runs delayed_job
+	@@singleton_process.run_process("bkg1");
+	
+	puts "singleton_process == #{SynthesiseDataJob.singleton_process()}";
+	puts "******* EXITING run_as_new_process";
+	Process.kill 9, Process.pid
+    end
+  end
+
+
+  def gen_measurement_for(mdist, simtime_s)
+    phasedt = simtime_s + mdist[:phase];
+    pt = phasedt/3600;  # perlin phase 
+    last_value = mdist[:avg_val] + (0.66*@n1d[pt] + 0.34*@n1d[pt*3.2] - 0.5);
+    # logger.debug(@last_value);
     mloc = @mlocs[mdist[:mloc_uuid]];
+    if mloc == nil then raise StandardError.new("nil mloc from hash."); end;
     mmsg = Measurement.new(
-	mlocation_id: mloc.id,
-	timestamp: Time.now,
+    	uuid: Random.uuid(),
+	m_location_id: mloc.id,
+	timestamp: @sim_start_time + simtime_s,
 	qtype: mdist[:template][:qtype],
 	v: last_value,
 	uom: mdist[:template][:uom]
@@ -45,6 +100,7 @@ class SynthesiseDataJob < ApplicationJob
     return mmsg
   end
 
+  SIM_TIME_PER_SECOND = (6*60);  # F2.14
   
   def perform(*args)
     Rails.logger.debug("Init synth with #{args}")
@@ -54,19 +110,29 @@ class SynthesiseDataJob < ApplicationJob
     @last_value = nil
     @last_message = nil
     
-    @n1d = Perlin::Noise.new(1, {:interval=>24 } )
+    @n1d = Perlin::Noise.new(1, {:interval=>3 } )  # 20-minute size features?
 
     Rails.logger.debug("Started synth")
-    simtime = 0.0
-    while self.keep_running and simtime<(1*24) do
+    simtime_s = 0.0
+    @sim_start_time = Time.now();
+    
+    while self.keep_running and simtime_s<(3600*12) do
     	data_frame = []
     	MLOC_DATA_DISTRIBUTIONS.each do |mdist|
-    	  mmsg = gen_measurement_for(mdist, simtime)
-	  SegmentMeasurementsChannel.send_measurement(mmsg)
-    	  @last_message = mmsg
+    	  mmsg = gen_measurement_for(mdist, simtime_s);
+    	  data_frame.push(mmsg);
     	end
-  	sleep(1)
-  	simtime = simtime + 1.0/10
+    	@last_message = data_frame;
+    	puts(simtime_s, "generated #{data_frame}")
+    	# save to DB
+    	for m in data_frame
+    	  m.save();
+    	end
+    	# prevent database from growing too large for demo.
+    	# TODO: would not do this in a long-lived application.
+    	Measurement.destroy_by(updated_at: ...(2.minutes.ago) );
+  	sleep(1);
+  	simtime_s = simtime_s + SIM_TIME_PER_SECOND;
     end
   end
   
