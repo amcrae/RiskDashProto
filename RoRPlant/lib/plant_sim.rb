c7501b0264bdc6e01512c94f06acaea7f96a38e3
Andrew McRae <ajmcrae@gmail.com>
2023-10-09 13:54:08 +1000

    Minor cleanup

--- RoRPlant/lib/plant_sim.rb
+++ RoRPlant/lib/plant_sim.rb
@@ -1,47 +1,49 @@
+# frozen_string_literal: true
 
+# Simple simulation which propagates input values through the dependency network,
+# maybe applying transformation specific to each component type along the way.
 class PlantSim
-    # Simple simulation which propagates input values through the dependency network,
-    # maybe applying transformation specific to each component type along the way.
 
-    def initialize(system_graph, sys_datetime, transfer_functions)
-        super()
-        @system_graph = system_graph;
-        @sys_time = sys_datetime;
-        @transfer_functions = transfer_functions
-    end
+  attr_accessor :sys_time
 
-    def sim_step(time_step)
-        stop_including = @sys_time + time_step;
-        measurements = Measurement.where("timestamp > ? and timestamp <= ? ", @sys_time, stop_including)
-        max_time = @sys_time
+  def initialize(system_graph, sys_datetime, transfer_functions)
+    super()
+    @system_graph = system_graph;
+    @sys_time = sys_datetime;
+    @transfer_functions = transfer_functions
+  end
 
-        latest_only = {}
-        for m in measurements
-            if m.timestamp > max_time then max_time  = m.timestamp end
-            if !latest_only.has_key?(m.m_location ) then
-                latest_only[m.m_location] = m;
-            else
-                last = latest_only[m.m_location]
-                if (last.timestamp < m.timestamp) && (last.qtype == m.qtype) then
-                    latest_only[m.m_location] = m;
-                end
-            end
+  def load_latest_measurements(start_exclusive, stop_inclusive)
+    measurements = Measurement.where("timestamp > ? and timestamp <= ? ", start_exclusive, stop_inclusive)
+    max_time = start_exclusive
+    latest_only = {};
+    measurements.each do |m|
+      if m.timestamp > max_time then max_time = m.timestamp end
+      if !latest_only.has_key?(m.m_location) then
+        latest_only[m.m_location] = m;
+      else
+        last = latest_only[m.m_location]
+        if (last.timestamp < m.timestamp) && (last.qtype == m.qtype) then
+          latest_only[m.m_location] = m;
         end
-        
-        propagate_inputs(latest_only.values, max_time);
-        # TODO: ....
-        @sys_time = stop_including
+      end
     end
+    return latest_only.values()
+  end
 
-    def propagate_inputs(measurements, max_time)
+  def sim_step(time_step)
+    stop_inclusive = @sys_time + time_step;
+    latest_measurements = load_latest_measurements(@sys_time, stop_inclusive)
+    propagate_inputs(latest_measurements, max_time);
+    # TODO: more?
+    @sys_time = stop_inclusive
+  end
 
-        for mmsg in measurements
-            puts mmsg
-            # TODO: ..
-
-        end
-    
+  def propagate_inputs(measurements, _max_time)
+    for mmsg in measurements
+      puts mmsg
+      # TODO: ..
     end
-
+  end
 
 end
