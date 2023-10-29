645b4193f3dbd8be3c144adf5bdb21974a6f4b90
Andrew McRae <ajmcrae@gmail.com>
2023-10-30 09:17:00 +1000

    Controller instrumented with instance vars for authn tests.

--- RoRPlant/app/controllers/plant_controller.rb
+++ RoRPlant/app/controllers/plant_controller.rb
@@ -3,22 +3,24 @@ class PlantController < ApplicationController
   # http_basic_authenticate_with name: "admin", password: "super", except: [:index, :show]
 
   def index
+    @usr_signed_in = user_signed_in?
+    @cur_usr = nil || current_user()
+
     hierarchy_sql = <<~HEREDOC
-	  WITH RECURSIVE cte_connect_by AS ( 
-	     SELECT 1 AS level, CAST( ('/' || shortname) AS VARCHAR(4000)) AS connect_by_path, s.* 
-	       FROM segments s WHERE id = 1
-	     UNION ALL
-	     SELECT level + 1 AS level, (connect_by_path || '/' || s.shortname) AS connect_by_path, s.* 
-	       FROM cte_connect_by r INNER JOIN segments s ON  r.id = s.parent_id
-	  )
-	  SELECT level, cte_connect_by.id, segtype, operational, cte_connect_by.shortname, parent_id, connect_by_path, 
-	  	 asset_id, assets.uuid as "asset_uuid", assets.shortname as "asset_name", assets.asset_type, assets.readiness
-	  FROM cte_connect_by LEFT OUTER JOIN assets ON cte_connect_by.asset_id = assets.id
-	  ORDER BY connect_by_path
+      WITH RECURSIVE cte_connect_by AS ( 
+         SELECT 1 AS level, CAST( ('/' || shortname) AS VARCHAR(4000)) AS connect_by_path, s.* 
+           FROM segments s WHERE id = 1
+         UNION ALL
+         SELECT level + 1 AS level, (connect_by_path || '/' || s.shortname) AS connect_by_path, s.* 
+           FROM cte_connect_by r INNER JOIN segments s ON  r.id = s.parent_id
+      )
+      SELECT level, cte_connect_by.id, segtype, operational, cte_connect_by.shortname, parent_id, connect_by_path, 
+      	 asset_id, assets.uuid as "asset_uuid", assets.shortname as "asset_name", assets.asset_type, assets.readiness
+      FROM cte_connect_by LEFT OUTER JOIN assets ON cte_connect_by.asset_id = assets.id
+      ORDER BY connect_by_path
     HEREDOC
-    # 	  ORDER BY connect_by_path
-    # level, connect_by_path path
-    plain_sql = " SELECT id, segtype, shortname, parent_id from segments ORDER BY id"
+    
+    # plain_sql = " SELECT id, segtype, shortname, parent_id from segments ORDER BY id"
     
     # @seg_tree = Segment.find_by_sql(hierarchy_sql)
     res = Segment.connection.select_all(hierarchy_sql).to_a();
