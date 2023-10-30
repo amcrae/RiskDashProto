1b9af4fea84d05a1199a8622ed05f2f884fe9766
Andrew McRae <ajmcrae@gmail.com>
2023-10-30 12:52:42 +1000

    Add browser-level Capybara testing of authn

--- RoRPlant/spec/requests/header_auth_spec.rb
+++ RoRPlant/spec/requests/header_auth_spec.rb
@@ -15,14 +15,15 @@ RSpec.describe PlantController, type: :controller do
   it "contains no User when given no auth headers" do
     # this get will run the controller.
     get :index # "http://127.0.0.1:3000/"
-    # but no other Rails middleware ran before it, so
-    # the 
+    # but no other Rails middleware ran before it, so the next line
+    # raises "Devise could not find the `Warden::Proxy` instance on your request environment"
     expect(assigns(:usr_signed_in)).to eq(false);
   end
 
   it "returns the user2 User via Devise when given user2 tokens" do
-    # the get will not route to middleware and controller.
-    get(root_path + "/?a=/MOCKPROXY/user2")
+    # the get will not route to middleware and controller
+    # because this is a :controller type spec/example.
+    get(root_path + "?a=/MOCKPROXY/user2")
     expect(assigns(:usr_signed_in)).to eq(true);
     expect(assigns(:cur_usr).email).to eq("user2@example.com")
   end
@@ -33,19 +34,26 @@ RSpec.describe "authn middleware request", type: :request do
   
   it "contains no User when given no auth headers" do
     get root_path # "http://127.0.0.1:3000/"
+    expect(session['warden.user.user.key']).to be_nil
     expect(response.body).to include("You are not logged in.")
   end
 
   it "returns the user2 User via Devise when given user2 tokens" do
-    get(root_path + "/?a=/MOCKPROXY/user2")
+    get(root_path + "?a=/MOCKPROXY/user2")
+    expect(response.status).to eq(200)
+    expect(session['warden.user.user.key']).not_to be_nil
     expect(response.body).to include("User McTwo")
   end
   
 end
 
-# RSpec.describe "authn results", type: :system do
-#   it "returns the user2 User via Devise when given user2 tokens" do
-#     visit(root_path + "/?a=/MOCKPROXY/user2");
-#     expect(page).to have_text("User McTwo");
-#   end
-# end
+RSpec.describe "Page authn results", type: :system do
+  before do
+    driven_by(:selenium_headless);
+  end
+
+  it "returns the user2 User via Devise when given user2 tokens", driver: :selenium_headless do
+    visit("http://127.0.0.1:3000" + root_path + "?a=/MOCKPROXY/user2");
+    expect(page).to have_text("User McTwo");
+  end
+end
