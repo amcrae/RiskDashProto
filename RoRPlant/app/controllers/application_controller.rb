fc2e8dbd6d0cf7a0c14821fe9e62754fdec6e828
Andrew McRae <ajmcrae@gmail.com>
2023-10-30 17:15:52 +1000

    Add logging of all Controller actions

--- RoRPlant/app/controllers/application_controller.rb
+++ RoRPlant/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :log_user_action!
+
+  def log_user_action!
+    ts = Time.now()
+    if current_user()
+      uname = "User " + current_user()[:email]
+    else
+      uname = "*UNKNOWN* user"
+    end
+    Rails.logger.debug("#{ts} *** #{uname} executing #{self.class} with #{params()}");
+  end
+
 end
