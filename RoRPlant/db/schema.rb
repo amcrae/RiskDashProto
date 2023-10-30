b8030073b230bb69b2f5b51b852e2a903bd60771
Andrew McRae <ajmcrae@gmail.com>
2023-10-30 14:34:49 +1000

    Add Devise Trackable module to User model

--- RoRPlant/db/schema.rb
+++ RoRPlant/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_023248) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_025737) do
   create_table "assets", force: :cascade do |t|
     t.string "uuid"
     t.string "shortname"
@@ -129,6 +129,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_023248) do
     t.datetime "updated_at", null: false
     t.string "role_name"
     t.string "full_name"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
