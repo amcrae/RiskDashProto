f50bd9c78c2a08ffd3bcbf1aee87a4d0967b234b
Andrew McRae <ajmcrae@gmail.com>
2023-10-31 11:52:31 +1000

    Fix incomplete test case so whole suite runs

--- RoRPlant/test/models/risk_assessment_test.rb
+++ RoRPlant/test/models/risk_assessment_test.rb
@@ -5,10 +5,16 @@ class RiskAssessmentTest < ActiveSupport::TestCase
   #   assert true
   # end
   def test_save
-    s = Segment.find(1)
-    otarg = MLocation.find(3)
-    r = RiskAssessment.new( uuid:'1-1-0', scope_segment_id: s.id, output_m_location_id: otarg.id, out_price: 100.00, out_currency: 'AUD', start_time: Time.now, lookahead: '1 day', end_time:(Time.now() + 1.days()), calc_alg:'basic_reliability' )
-
+    s = Segment.find_by(uuid: "1-2b01-0")
+    otarg = MLocation.find_by(uuid: "1-2b01-2-1")
+    r = RiskAssessment.new(
+      uuid: '1-1-0', scope_segment_id: s.id, output_m_location_id: otarg.id, 
+      out_price: 100.00, 
+      out_currency: 'AUD', 
+      start_time: Time.now, lookahead: '1 day', end_time: (Time.now() + 1.days()),
+      calc_alg: 'basic_reliability'
+    )
+    r.save()
   end
   
 end
