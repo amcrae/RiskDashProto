ccc2f05e834dd9a6f68d524af91a498cb366beb2
Andrew McRae <ajmcrae@gmail.com>
2023-10-31 17:12:30 +1000

    More tests on header authentication

--- RoRPlant/spec/requests/header_auth_spec.rb
+++ RoRPlant/spec/requests/header_auth_spec.rb
@@ -31,20 +31,56 @@ RSpec.describe PlantController, type: :controller do
 end
 
 RSpec.describe "authn middleware request", type: :request do
-  
+  # fixtures :users # does not work because 'password' is a property not a DB column
+
+  let!(:u_one) { 
+    u1 = User.new(
+      auth_type: "LOCAL",
+      email: "user1@example.com", password: "scoTTY", full_name: "Montgomery Scott", role_name: "TECHNICIAN"
+    );
+    u1.save();
+    u1.reload();
+    u1
+  }
+
   it "contains no User when given no auth headers" do
     get root_path # "http://127.0.0.1:3000/"
     expect(session['warden.user.user.key']).to be_nil
     expect(response.body).to include("You are not logged in.")
   end
 
-  it "returns the user2 User via Devise when given user2 tokens" do
+  it "returns the new user2 User via Devise when given user2 tokens" do
     get(root_path + "?a=/MOCKPROXY/user2")
-    expect(response.status).to eq(200)
+
     expect(session['warden.user.user.key']).not_to be_nil
+    u2_ll1 = User.find_by(email: "user2@example.com").last_sign_in_at
+    expect(u2_ll1).not_to be_nil
+    expect(response.status).to eq(200)
     expect(response.body).to include("User McTwo")
   end
-  
+
+  it "returns the old user2 User via Devise when given user2 tokens" do
+    # users(:u_two)
+    # puts "Using LETted object #{u_one}"
+    # u2_ll0 = u_one().last_sign_in_at
+    u2_0 = User.find_by(email: "user2@example.com")
+    u2_ll0 = u2_0.last_sign_in_at
+    puts "last_sign_in_at #{u2_ll0}"
+    expect(u2_ll0).not_to be_nil
+
+    get(root_path + "?a=/MOCKPROXY/user2")
+
+    expect(session['warden.user.user.key']).not_to be_nil
+    
+    puts "lookupid #{u2_0.id}"
+    u2_1 = User.find(u2_0.id)
+    u2_ll1 = u2_1.last_sign_in_at
+    expect(u2_ll1).to satisfy("signin timestamp was incremented") { |x| x > u2_ll0}
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include("User McTwo")
+  end
+
 end
 
 RSpec.describe "Page authn results", type: :system do
