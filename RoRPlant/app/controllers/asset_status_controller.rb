6cbdac90b18cce0a6a779a2cb1b3b2d20ed95f2f
Andrew McRae <ajmcrae@gmail.com>
2023-11-03 18:50:51 +1000

    Try to render authorisation error as flash message

--- RoRPlant/app/controllers/asset_status_controller.rb
+++ RoRPlant/app/controllers/asset_status_controller.rb
@@ -4,34 +4,52 @@ class AssetStatusController < ApplicationController  # ActionController::API
   before_action :authenticate_user!, except: %i[ show ]
 
   def sabotage
-    asset_id = params[:asset_id];
-    Rails.logger.debug("Gonna sabotage #{asset_id} !");
-    @asset = Asset.find(asset_id);
-    @asset.readiness = 'FAILED';
-    if @asset.segment then
-      @segment = @asset.segment;
-      @segment.operational = "OFFLINE";
-      @segment.save()
+    begin
+      asset_id = params[:asset_id];
+      @asset = Asset.find(asset_id);
+      authorize! :sabotage, @asset
+      Rails.logger.debug("Gonna sabotage #{asset_id} !");
+      @asset.readiness = 'FAILED';
+      if @asset.segment then
+        @segment = @asset.segment;
+        @segment.operational = "OFFLINE";
+        @segment.save()
+      end
+      @asset.save();
+      render partial: "asset_status/show", locals: { asset: @asset }
+    rescue CanCan::AccessDenied => e
+      render partial: "asset_status/show", locals: { asset: @asset }
+      flash.now[:alert] = "Sabotage not permitted for user. #{e}"
+      # render turbo_stream: [
+      #  turbo_stream.replace("flash-messages", partial: "application/flash_message")
+      # ]
+      # render partial: "application/flash_message"
     end
-    @asset.save();
-    render partial: "asset_status/show", locals: { asset: @asset }
   end
 
   def repair
-    asset_id = params[:asset_id];
-    Rails.logger.debug("Gonna repair #{asset_id} !");
-    @asset = Asset.find(asset_id);
-    @asset.readiness = 'SERVICEABLE';
-    @asset.save();  	
-    if @asset.segment then
-      @segment = @asset.segment;
-      @segment.operational = "RUNNING";
-      @segment.save()
+    begin
+      asset_id = params[:asset_id];
+      @asset = Asset.find(asset_id);
+      authorize! :repair, @asset
+      Rails.logger.debug("Gonna repair #{asset_id} !");
+      @asset.readiness = 'SERVICEABLE';
+      @asset.save();  	
+      if @asset.segment then
+        @segment = @asset.segment;
+        @segment.operational = "RUNNING";
+        @segment.save()
+      end
+    rescue CanCan::AccessDenied
+      flash[:alert] = "Repair not permitted for user."
     end
     render partial: "asset_status/show", locals: { asset: @asset }
   end
 
   def show
+    asset_id = params[:asset_id];
+    @asset = Asset.find(asset_id);    
+    authorize! :read, @asset
   end
   
 end
