1d00293754a030b22e20b15eb2800d0a8f357622
Andrew McRae <ajmcrae@gmail.com>
2023-11-06 20:50:07 +1000

    Prevent Devise error on sign-in form. * Devise by default expects non-authenticated user   when asking to sign-in. Given the user's intention   is to authenticate as a different user, the easiest   solution is to do a sign-out of their ext-auth user. + If a login for a user different to the already   logged-in user happens (i.e. without a login form)   this will be detected and logged.
    It is normal for an ext auth session to re-login
     as a DB-auth user,  so there is no error or forced
     sign-out in this case.

--- RoRPlant/app/controllers/session_controller.rb
+++ RoRPlant/app/controllers/session_controller.rb
@@ -1,17 +1,29 @@
 class SessionController < Devise::SessionsController
+  before_action :require_no_authentication, :only => []
+
+  def new
+    # sign-out matches Devise default requirement of user being
+    # non-authenticated when trying to sign-in.
+    Rails.logger.debug("Session custom create: doing sign_out of #{current_user&.email} to allow conventional sign in.")
+    sign_out
+    super
+  end
 
   def create
-    Rails.logger.debug("Session custom create")
+    # Rails.logger.debug("Session custom create: ... #{current_user.to_json}")
     upn = params().fetch('user', nil)&.fetch('email', nil);
     if upn then
       u = User.find_by(email: upn);
-      if u.auth_type != 'LOCAL' then
-        sign_out
+      if u&.auth_type != 'LOCAL' then
         flash[:alert] = t(:auth_method_not_permitted)
         redirect_back(fallback_location: root_path)
         return
       end
+      if upn != (current_user&.email) then
+        Rails.logger.debug("Session custom create: sign-in post user == #{upn}, current_user == #{current_user&.email}")
+      end
     end  
+    # Rails.logger.debug("Session custom create: invoking super.")
     super
   end
 
