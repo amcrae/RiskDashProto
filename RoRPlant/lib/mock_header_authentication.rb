38385931f7d7c573652cef95e242ba6f24bc1cad
Andrew McRae <ajmcrae@gmail.com>
2023-11-07 18:14:48 +1000

    Implement interfaces changes from module. * Changed hash for user details * Changed contents of user_roles + Added a simulated directory lookup for user roles   by GETting a local JSON file, config in auth.yaml.

--- RoRPlant/lib/mock_header_authentication.rb
+++ RoRPlant/lib/mock_header_authentication.rb
@@ -2,9 +2,11 @@
 
 require 'digest'
 require 'base64'
+require 'json'
+require 'open-uri'
 
 # Subclass of HeaderAuthentication which provides custom functions
-# for verifying signatures of the hombrewed symmetric key signature 
+# for verifying signatures of the homebrewed symmetric key signature 
 # scheme developed for testing.
 class MockHeaderAuthentication
   include HeaderAuthentication
@@ -16,7 +18,7 @@ class MockHeaderAuthentication
     @@role_mapping ||= Rails.application.config_for(:authorisation)[:provider_to_app];
   end
 
-  def self.nop(i, extraction_hash, header_value, req, sesh, user_template, user_roles)
+  def self.nop(i, extraction_hash, header_value, req, sesh, user_info)
     return header_value
   end
 
@@ -38,38 +40,59 @@ class MockHeaderAuthentication
   end
 
   # get user attributes from the mock access-token during prototyping.
-  def self.get_user_template(i, extraction_hash, header_value, req, sesh, user_template, user_roles)
+  def self.get_user_template(i, extraction_hash, header_value, req, sesh, user_info)
     data, sig_rcvd = decode_homebrew(header_value) 
     user_hash = JSON.parse(data)
-    user_template.update(user_hash)
+    user_info[:user_attributes].update(user_hash)
     return user_hash
   end
 
   @@ext_to_native_role = ->(ext_name) {
-    uc = ext_name.upcase.to_sym
-    if @@role_mapping.has_key?(uc) then
-      return @@role_mapping[uc][0]
+    # puts ext_name, @@role_mapping
+    if ext_name.include?('_') then
+      file_key = ext_name.upcase.to_sym
+    else 
+      file_key = ext_name.to_sym
+    end
+    if @@role_mapping.has_key?(file_key) then
+      return @@role_mapping[file_key][0]
     else
       return ext_name
     end
   }
 
+  def self.query_directory_for_roles(extraction_hash, upn)
+    user_portion = upn.split('@')[0]
+    uri = extraction_hash[:directory_formula].sub("$USER", user_portion)
+    response = OpenURI.open_uri(uri)
+    groups = JSON.parse(response.read())
+    response.close();
+    answer = [];
+    for grp_obj in groups["value"]
+      answer.append(grp_obj["id"])
+    end
+    # puts "query_directory_for_roles returns #{answer}"
+    return answer
+  end
+
   # The assumption is the roles returned are the ones used by
   # this web application, so any mapping of external roles 
   # into native app roles must also happen here.
-  def self.mock_extract_roles(i, extraction_hash, header_value, req, sesh, user_template, user_roles)
+  def self.mock_extract_roles(i, extraction_hash, header_value, req, sesh, user_info)
+    # Can extrac roles from token if given.
     data, sig_rcvd = decode_homebrew(header_value) 
     user_hash = JSON.parse(data)
     given_ext_roles = user_hash['memberOf']
-    given_native_roles = given_ext_roles.map(&@@ext_to_native_role)
-    user_roles.clear()
-    user_roles.concat(given_native_roles)
-    return given_native_roles
+    # Can query LDAP (etc) for more info.
+    given_ext_roles = self.query_directory_for_roles(extraction_hash, user_info[:upn])
+    user_info[:ext_roles_array] = given_ext_roles
+    # puts "given_ext_roles := #{given_ext_roles}"
+    return given_ext_roles
   end
 
-  def self.verify_match_to_template(i, extraction_hash, header_value, req, sesh, user_template, user_roles)
+  def self.verify_match_to_template(i, extraction_hash, header_value, req, sesh, user_info)
     puts "verify_match_to_template"
-    return header_value == user_template['upn']
+    return header_value == user_info[:upn]
   end
 
   def self.user_from_upn(upn)
@@ -92,17 +115,19 @@ class MockHeaderAuthentication
     return account.auth_type == 'EXTERNAL'
   end
 
-  def self.set_user_roles(role_array, account)
+  def self.set_user_roles(ext_role_array, account)
+    given_native_roles = ext_role_array.map(&@@ext_to_native_role)
+    puts "given_native_roles := #{given_native_roles}"
     # In this basic user model they only had 1 role.
-    account.role_name = role_array[0]
+    account.role_name = given_native_roles[0]
   end
 
-  def self.new_user(user_template) 
+  def self.new_user(user_info) 
     init_pw = Digest::SHA1.hexdigest(Random.bytes(8));
     account = User.new(
       auth_type: "EXTERNAL", # authentication continues to be by headers
-      email: user_template['mail'], 
-      full_name: user_template['fullname'],
+      email: user_info[:user_attributes]['mail'], 
+      full_name: user_info[:user_attributes]['fullname'],
       password: init_pw # Not used due to external auth, but cannot be NULL.
     );
     return account;
