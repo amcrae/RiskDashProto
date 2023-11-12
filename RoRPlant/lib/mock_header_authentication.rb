e2a55dd4add6b97b09a993da0ed14ef401476ed9
Andrew McRae <ajmcrae@gmail.com>
2023-11-12 18:14:37 +1000

    Class changed to module * More options for init signature and superclass e.g Devise Strategy. * Class methods written as self.x had to be renamed to allow extend. * class variable previously defined by parent module   has been moved to specific module since it   is for mapping of ext roles to app domain.

--- RoRPlant/lib/mock_header_authentication.rb
+++ RoRPlant/lib/mock_header_authentication.rb
@@ -1,24 +1,29 @@
 # frozen_string_literal: true
 
-# Class to implement functions specific to the 
-# homebrewed mock header authentication scheme.
-class MockHeaderAuthentication
-  include HeaderAuthentication
+require 'header_authentication'
 
-  def self.nop(i, extraction_hash, header_value, req, sesh, user_info)
+# Class methods to implement functions specific to the 
+# homebrewed mock header authentication scheme.
+module MockHeaderAuthentication
+  
+  def init_class_vars()
+    @@role_mapping ||= Rails.application.config_for(:authorisation)[:provider_to_app];
+  end
+
+  def nop(i, extraction_hash, header_value, req, sesh, user_info)
     return header_value
   end
 
-  def self.mock_key(*args)
+  def mock_key(*args)
     return 'SECRET'
   end
 
-  def self.decode_homebrew(header_value)
+  def decode_homebrew(header_value)
     decoded = Base64.decode64(header_value);
     return decoded.split('|')
   end
 
-  def self.mock_sig_validation(header_value, signing_key)
+  def mock_sig_validation(header_value, signing_key)
     puts "mock_sig_validation"
     data, sig_rcvd = decode_homebrew(header_value) 
     prefixed = signing_key + '|' + data
@@ -27,7 +32,7 @@ class MockHeaderAuthentication
   end
 
   # get user attributes from the mock access-token during prototyping.
-  def self.get_user_template(i, extraction_hash, header_value, req, sesh, user_info)
+  def get_user_template(i, extraction_hash, header_value, req, sesh, user_info)
     data, sig_rcvd = decode_homebrew(header_value) 
     user_hash = JSON.parse(data)
     user_info[:user_attributes].update(user_hash)
@@ -48,7 +53,7 @@ class MockHeaderAuthentication
     end
   }
 
-  def self.query_directory_for_roles(extraction_hash, upn)
+  def query_directory_for_roles(extraction_hash, upn)
     user_portion = upn.split('@')[0]
     uri = extraction_hash[:directory_formula].sub("$USER", user_portion)
     body = nil
@@ -74,7 +79,7 @@ class MockHeaderAuthentication
   # The assumption is the roles returned are the ones used by
   # this web application, so any mapping of external roles 
   # into native app roles must also happen here.
-  def self.mock_extract_roles(i, extraction_hash, header_value, req, sesh, user_info)
+  def mock_extract_roles(i, extraction_hash, header_value, req, sesh, user_info)
     # Can extrac roles from token if given.
     data, sig_rcvd = decode_homebrew(header_value) 
     user_hash = JSON.parse(data)
@@ -86,17 +91,17 @@ class MockHeaderAuthentication
     return given_ext_roles
   end
 
-  def self.verify_match_to_template(i, extraction_hash, header_value, req, sesh, user_info)
+  def verify_match_to_template(i, extraction_hash, header_value, req, sesh, user_info)
     puts "verify_match_to_template"
     return header_value == user_info[:upn]
   end
 
-  def self.user_from_upn(upn)
+  def user_from_upn(upn)
     puts "user_from_upn"
     return User.find_by(email: upn)
   end
 
-  def self.upn_from_user(account)
+  def upn_from_user(account)
     puts "upn_from_user"
     if account.is_a?(Hash) then
       return account[:email]
@@ -107,18 +112,18 @@ class MockHeaderAuthentication
     raise TypeError.new("Unexpected user account type #{account.class}")
   end
 
-  def self.user_has_ext_authn(account)
+  def user_has_ext_authn(account)
     return account.auth_type == 'EXTERNAL'
   end
 
-  def self.set_user_roles(ext_role_array, account)
+  def set_user_roles(ext_role_array, account)
     given_native_roles = ext_role_array.map(&@@ext_to_native_role)
     puts "given_native_roles := #{given_native_roles}"
     # In this basic user model they only had 1 role.
     account.role_name = given_native_roles[0]
   end
 
-  def self.new_user(user_info) 
+  def new_user(user_info) 
     init_pw = Digest::SHA1.hexdigest(Random.bytes(8));
     account = User.new(
       auth_type: "EXTERNAL", # authentication continues to be by headers
@@ -128,5 +133,5 @@ class MockHeaderAuthentication
     );
     return account;
   end
-
+  
 end
