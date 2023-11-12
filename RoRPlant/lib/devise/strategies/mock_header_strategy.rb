5f25a7bfddfb8fe7e2fe4f46faba5bdf748602db
Andrew McRae <ajmcrae@gmail.com>
2023-11-12 18:22:03 +1000

    Make strategy using header modules. * Strategy implementation currently inherited   from the general module as anything scheme specific   is being done by the custom class methods. * Change from class to module has side-effects   on how class methods+vars can be accessed,   such as add_to_warden_method.
    TODO: Config paramter ("Mock") not in best place.

--- RoRPlant/lib/devise/strategies/mock_header_strategy.rb
+++ RoRPlant/lib/devise/strategies/mock_header_strategy.rb
@@ -4,32 +4,28 @@ require 'digest'
 require 'base64'
 require 'json'
 require 'open-uri'
-require_relative '../../header_authentication'
+require_relative '../../mock_header_authentication'
 
 # Subclass of Warden Strategy (via Devise) which provides custom functions
 # for verifying signatures of the homebrewed symmetric key signature 
 # scheme developed for testing.
 class MockHeaderStrategy < Devise::Strategies::Base
+  include HeaderAuthentication
+  extend MockHeaderAuthentication
 
   @@role_mapping = nil
-
-  def initialize
-    super
-    @impl = MockHeaderAuthentication.new()
+  # a particular constructor sig is expected by Warden
+  def initialize(*args)
+    super(*args)
+    #@impl_class = MockHeaderAuthentication
+    #@impl = @impl_class.new();
+    configure_functions("Mock")
     @@role_mapping ||= Rails.application.config_for(:authorisation)[:provider_to_app];
-  end
-
-  def valid? 
-    @impl.valid?
-  end
-
-  def authenticate! 
-    @impl.authenticate!
   end
 
   # To be called from Rails application-level config to install the custom auth functions.
   def self.add_to_warden(config_name)
-    @@add_to_warden_method.call(self, config_name)
+    HeaderAuthentication.add_to_warden_method(self, config_name)
   end
 
 end
