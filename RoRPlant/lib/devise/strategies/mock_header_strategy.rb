293713d4e79b1866afb803097720348634336252
Andrew McRae <ajmcrae@gmail.com>
2023-11-13 19:05:55 +1000

    Cleanup of Strategy implementation

--- RoRPlant/lib/devise/strategies/mock_header_strategy.rb
+++ RoRPlant/lib/devise/strategies/mock_header_strategy.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-require 'digest'
-require 'base64'
-require 'json'
-require 'open-uri'
-require_relative '../../mock_header_authentication'
+require 'mock_header_authentication'
+require 'devise/models/plant_user_intf'
 
 # Subclass of Warden Strategy (via Devise) which provides custom functions
 # for verifying signatures of the homebrewed symmetric key signature 
@@ -12,15 +9,14 @@ require_relative '../../mock_header_authentication'
 class MockHeaderStrategy < Devise::Strategies::Base
   include HeaderAuthentication
   extend MockHeaderAuthentication
+  extend PlantUserIntf
 
   @@role_mapping = nil
+
   # a particular constructor sig is expected by Warden
   def initialize(*args)
     super(*args)
-    #@impl_class = MockHeaderAuthentication
-    #@impl = @impl_class.new();
     configure_functions("Mock")
-    @@role_mapping ||= Rails.application.config_for(:authorisation)[:provider_to_app];
   end
 
   # To be called from Rails application-level config to install the custom auth functions.
