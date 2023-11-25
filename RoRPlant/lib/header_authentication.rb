75f76edaa4df5d481d247c471cef5bc2a0338708
Andrew McRae <ajmcrae@gmail.com>
2023-11-25 17:33:31 +1000

    Move generic middleware code into top module. * Minimises work for application developer. + Separate the User account refresh into new method   as opportunity for application to override.

--- RoRPlant/lib/header_authentication.rb
+++ RoRPlant/lib/header_authentication.rb
@@ -297,4 +297,91 @@ module HeaderAuthentication
     Warden::Strategies.add(strategy_name, subclass);
   end
   
+
+  # ----
+  # Generic implementation of Rack middleware call(env).
+  # ----
+
+  def call(env)
+    req = Rack::Request.new(env);
+
+    # reconstructed = reconstruct_headers(env)
+    # sesh = req.session
+    sesh = env['rack.session']
+
+    all_present = false
+    signout_detected = false
+    begin
+      all_present, signout_detected = find_required_headers(req, sesh)
+    rescue StandardError => e
+      res = Rack::Response.new("Web server configuration problem #{e}", 500, {})
+      return [res.status, res.headers, res.body]
+    end
+
+    if !all_present then
+      Rails.logger.info("HeaderAuthentication found no auth headers.")
+      if signout_detected then
+        old_account_obj = sesh[SESS_KEY_HA_AUTH_USER];
+        # The :get_upn_from_user_function may be passed a Hash of the fields
+        # instead of the original ActiveRecord account object,
+        # due to web session state being serialised to storage.
+        old_upn = self.class.get_upn_from_app_user(old_account_obj)
+        Rails.logger.info("External signout detected from old session of #{old_upn}, clearing auth session variables.")
+        clear_session_vars(req, sesh);
+      end
+      return @app.call(env)
+    end
+
+    # In case of a previously externally-authorised user,
+    # check if loss of headers has occurred or the headers user
+    # has changed.
+    userid_changed = false
+    # This will duplicate some work also done later in the chain by `authenticate!`
+    #  during initial authentication of a previously unauthenticated session.
+    # However rechecking signatures of all requests will require this
+    #  work to be done here anyhow, since `authenticate!` is only called once.
+    user_info = identify_user(req, sesh); # performs sig checks too.
+    if user_info != nil && user_info[:upn] != nil then
+      Rails.logger.info("Middleware identified user as #{user_info}")
+      req_upn = user_info[:upn].downcase();
+      if sesh[SESS_KEY_HA_AUTH_UPN] != nil then
+        # Session already existed.
+        session_upn = sesh[SESS_KEY_HA_AUTH_UPN].downcase()
+        current_session_user = self.class.load_app_user_by_upn(session_upn);
+        ext_auth = self.class.app_user_needs_headerauth?(current_session_user)
+        if ext_auth then 
+          if req_upn == nil 
+            userid_changed = true
+            Rails.logger.info("External signout detected from old session of #{session_upn}, clearing auth session variables.")
+          elsif req_upn != session_upn then
+            userid_changed = true
+            Rails.logger.warn("External headers show different user than current login of #{session_upn}, clearing auth session variables.")
+          else
+            Rails.logger.info("Refresh user details from token")
+            refresh_user_details(user_info, current_session_user);
+            sesh[SESS_KEY_HA_AUTH_USER] = current_session_user
+          end
+        end
+      end
+      if userid_changed
+        # Effects controlled mainly by the string array in 
+        #  'header_authentication.yaml' / {config} / signout_erases_session_vars:
+        #  which must include 'warden.user.user.key' in order to
+        #  emulate a logout without having access to the Devise helper for sign_out().
+        clear_session_vars()
+      end
+    end
+  
+    # Continue request handling chain.
+    return @app.call(env)
+  end
+  
+  # Called from generic middleware `call` method and separated out as 
+  # an option to be overridden by host application.
+  def refresh_user_details(user_info, current_session_user)
+    self.class.update_app_user(user_info, current_session_user);
+    self.class.set_app_user_roles(user_info[:ext_roles_array], current_session_user);
+    current_session_user.save()
+  end
+  
 end
--- RoRPlant/lib/mock_auth_middleware.rb
+++ RoRPlant/lib/mock_auth_middleware.rb
@@ -15,71 +15,14 @@ class MockAuthMiddleware
     configure_mw(app, configname);
   end
 
-  def call(env)
-    req = Rack::Request.new(env);
+  # generic call implementation is mixed-in from HeaderAuthentication.
 
-    # reconstructed = reconstruct_headers(env)
-    # sesh = req.session
-    sesh = env['rack.session']
-
-    all_present = false
-    signout_detected = false
-    begin
-      all_present, signout_detected = find_required_headers(req, sesh)
-    rescue StandardError => e
-      res = Rack::Response.new("Web server configuration problem #{e}", 500, {})
-      return [res.status, res.headers, res.body]
-    end
-
-    if !all_present then
-      Rails.logger.info("HeaderAuthentication found no auth headers.")
-      if signout_detected then
-        old_account_obj = sesh[SESS_KEY_HA_AUTH_USER];
-        # The :get_upn_from_user_function may be passed a Hash of the fields
-        # instead of the original ActiveRecord account object,
-        # due to web session state being serialised to storage.
-        old_upn = self.class.get_upn_from_app_user(old_account_obj)
-        Rails.logger.info("External signout detected from old session of #{old_upn}, clearing auth session variables.")
-        clear_session_vars(req, sesh);
-      end
-      return @app.call(env)
-    end
-
-    # in case of a previously externally-authorised user
-    # check if loss of headers has occurred or the headers user
-    # has changed.
-    userid_changed = false
-    user_info = identify_user(req, sesh); # performs sig checks too.
-    if user_info != nil && user_info[:upn] != nil then
-      Rails.logger.info("Middleware identified user as #{user_info}")
-      req_upn = user_info[:upn].downcase();
-      if sesh[SESS_KEY_HA_AUTH_UPN] != nil then
-        # Session already existed.
-        session_upn = sesh[SESS_KEY_HA_AUTH_UPN].downcase()
-        current_session_user = self.class.load_app_user_by_upn(session_upn);
-        ext_auth = self.class.app_user_needs_headerauth?(current_session_user)
-        if ext_auth then 
-          if req_upn == nil 
-            userid_changed = true
-            Rails.logger.info("External signout detected from old session of #{session_upn}, clearing auth session variables.")
-          elsif req_upn != session_upn then
-            userid_changed = true
-            Rails.logger.warn("External headers show different user than current login of #{session_upn}, clearing auth session variables.")
-          else
-            Rails.logger.info("Refresh user details from token")
-            self.class.update_app_user(user_info, current_session_user);
-            self.class.set_app_user_roles(user_info[:ext_roles_array], current_session_user);
-            current_session_user.save()
-            sesh[SESS_KEY_HA_AUTH_USER] = current_session_user
-          end
-        end
-      end
-      if userid_changed
-        clear_session_vars()
-      end
-    end
-  
-    return @app.call(env)
+  # Called from inherited call and overridden here with identical code 
+  # just as an implementation example.
+  def refresh_user_details(user_info, current_session_user)
+    self.class.update_app_user(user_info, current_session_user);
+    self.class.set_app_user_roles(user_info[:ext_roles_array], current_session_user);
+    current_session_user.save()
   end
 
 end
