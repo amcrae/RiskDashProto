f10596d20e6a06a78ee3f1e33876955e44773d96
Andrew McRae <ajmcrae@gmail.com>
2023-11-25 20:25:57 +1000

    Alternative point for refreshing User attributes.

--- RoRPlant/app/controllers/application_controller.rb
+++ RoRPlant/app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # # Middleware is currently used instead to do updates.
+  # before_action :refresh_user!
+
   before_action :log_user_action!
+
+  # This point is an alternative place (aside from middleware)
+  #  to update the details of the User from the external source.
+  def refresh_user!
+    ts = Time.now()
+    if session.has_key?(HeaderAuthentication::SESS_KEY_HA_AUTH_USER) \
+      && current_user()
+    then
+      account = session[HeaderAuthentication::SESS_KEY_HA_AUTH_USER];
+      current_user().full_name = account.full_name
+      current_user().role_name = account.role_name
+      Rails.logger.info("#{ts} User attributes for #{account.email} updated from Devise current_user.");
+    end
+  end
 
   def log_user_action!
     ts = Time.now()
